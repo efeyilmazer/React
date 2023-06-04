ed4bd4276feca79c007caf836f51d3a656afd8cf
Efe Yılmazer <efekan1232@gmail.com>
2023-06-04 04:56:42 +0300

    Create Header.js

---
+++ spending-money-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function Header({money}) {
+    return (
+        <>
+        <div>Harcayacak ${money} paranız var.</div>
+        
+        </>
+    )
+}
+
+export default Header;
