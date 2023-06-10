48a7c179edbcf2867362e5d6dbbbbd4a698c8c53
Efe Yılmazer <efekan1232@gmail.com>
2023-06-11 02:40:27 +0300

    remaining

---
+++ using-api/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SearchHeader from './SearchHeader'
+
+function App() {
+
+
+  return (
+<div className='App'>
+  <SearchHeader search={handleSubmit}/>
+</div>
+  )
+}
+
+export default App
---
+++ using-api/src/SeachHeader.jsx
@@ -0,0 +1,17 @@
+function SearchHeader() {
+
+    const handleFormSubmit = () => {
+
+    }
+
+    return ( 
+        <div>
+            <form onSubmit={handleFormSubmit}>
+                <label>Ara</label>
+                <input />
+            </form>
+        </div>
+     );
+}
+
+export default SearchHeader;
