86e0c14a74dee21327197529db70f01a6ff4e943
efeyilmazer <84992900+efeyilmazer@users.noreply.github.com>
2024-11-17 20:03:29 +0300

    updated

--- mini-portfolio/src/components/Navbar.jsx
+++ mini-portfolio/src/components/Navbar.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import Ben from "../img/Ben.jpeg"
+import { FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className='bg-white w-screen h-20 flex justify-center items-center drop-shadow-md shadow-inner mt-8 gap-4'>
-            <h1 className='text-3xl text-end w-[250px] font-display'>&lt; Efe <br/> Yılmazer</h1>
+<div className='flex flex-col items-center'>
+<div className='bg-white w-screen h-20 flex justify-center items-center drop-shadow-md shadow-inner mt-8 gap-4'>
+            <h1 className='text-3xl text-end w-[250px] font-display max-sm:text-xl'>&lt; Efe <br/> Yılmazer</h1>
             <img className='rounded-full w-32 shadow-md shadow-current' src={Ben}></img>
-            <h1 className='text-3xl w-[250px] font-display'>React Developer /&gt;</h1>
+            <h1 className='text-3xl w-[250px] font-display max-sm:text-xl'>React Developer /&gt;</h1>
             <div className='absolute right-2 bottom-0'>
-            <a href="github.com/efeyilmazer" target='blank' class=" font-display mt-8 relative w-[max-content] font-mono 
-before:absolute before:inset-0 before:animate-typewriter before:bg-white after:absolute after:inset-0 after:w-[0.125em] after:animate-caret after:bg-black hover:text-[17px] transition-all">&gt; github.com/efeyilmazer</a>
+            <a href="github.com/efeyilmazer" target='blank' class=" font-display mt-8 relative w-[max-content] font-mono before:absolute before:inset-0 before:animate-typewriter before:bg-white after:absolute after:inset-0 after:w-[0.125em] after:animate-caret after:bg-black hover:text-[17px] transition-all max-sm:invisible animate-pulse">&gt; github.com/efeyilmazer</a>
             </div>
     </div>
+    <div className='flex items-center justify-center w-[720px] text-xl'>
+      <span>
+        <button href="#tecrube" className=' bg-white w-32 h-12 rounded-b-2xl hover:drop-shadow-xl hover:scale-110 transition-all drop-shadow-sm active:scale-100'><a href="#tecrube">Tecrübelerim</a></button>
+        <button className=' mr-16 ml-4 bg-white w-32 h-12 rounded-b-2xl hover:drop-shadow-xl hover:scale-110 transition-all drop-shadow-sm active:scale-100'><a href="#freelance">Freelance</a></button>
+      </span>
+      <span>
+        <button className=' ml-16 mr-4 bg-white w-32 h-12 rounded-b-2xl hover:drop-shadow-xl hover:scale-110 transition-all drop-shadow-sm active:scale-100'><a href="#projelerim">Projelerim</a></button>
+        <button className=' bg-white w-32 h-12 rounded-b-2xl hover:drop-shadow-xl hover:scale-110 transition-all drop-shadow-sm active:scale-100'><a href="#iletisim">İletişim</a></button>
+      </span>
+    </div>
+</div>
 
 
 
--- mini-portfolio/src/pages/Main.jsx
+++ mini-portfolio/src/pages/Main.jsx
@@ -7,36 +7,59 @@ import TKBB from "../img/tkbb.jpg"
 import Payna from "../img/payna.jpg"
 import NU from "../img/nisantasilogo.png"
 
+import { FaHtml5, FaPhp } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { BiLogoJavascript, BiLogoTypescript } from "react-icons/bi";
+import { FaReact } from "react-icons/fa";
+import { TbBrandRedux } from "react-icons/tb";
+import { FaJava } from "react-icons/fa";
+import { BsFiletypeSql } from "react-icons/bs";
+import { DiDotnet } from 'react-icons/di'
+
 const Main = () => {
   return (
     <div>
         <Navbar />
+      <div>
         <div className='flex flex-col items-center w-screen h-scroll bg-white/10 pt-14'>
           <h1 className='text-2xl pb-4 font-display'>Merhaba, Ben Efe!</h1>
-          <p className='w-[1200px] text-xl text-center pb-8'>2019 yılından itibaren Front-End kapsamında kendimi bir Software Developer gözetimi ve takibi altında geliştirmekteyim. <br/> Kendimi geliştirdiğim süre boyunca bu alanda 150'den fazla proje geliştirdim. Daha önce iki startup projesinde Front-End developer olarak yer aldım. Haftalık tasklar şeklinde çalıştığım projede takım çalışmasını ve sahada proje geliştirme deneyimini elde ettim. <br/> Hazırlık sınıfını okul birincisi olarak bitirdim ve ileri Düzey İngilizce bilgisine sahibim.</p>
-          <div className='flex mb-24 bg-white/60 rounded-3xl p-8 drop-shadow-md shadow-inner'>
-            <img src={NU} className='w-48'></img>
+          <p className='w-[1200px] text-xl text-center pb-12 max-sm:w-[450px] max-sm:text-pretty'>2019 yılından itibaren Front-End kapsamında kendimi bir Software Developer gözetimi ve takibi altında geliştirmekteyim. <br/> Kendimi geliştirdiğim süre boyunca bu alanda 150'den fazla proje geliştirdim. Daha önce iki startup projesinde Front-End developer olarak yer aldım. Haftalık tasklar şeklinde çalıştığım projede takım çalışmasını ve sahada proje geliştirme deneyimini elde ettim. <br/> Hazırlık sınıfını okul birincisi olarak bitirdim ve ileri Düzey İngilizce bilgisine sahibim.</p>
+          <div className='max-sm:h-48 flex mb-12 bg-white/60 rounded-3xl p-8 drop-shadow-md shadow-inner'>
+            <img src={NU} className='w-48 max-sm:h-32'></img>
             <span className='flex justify-center flex-col'>
               <span className='flex justify-between items-center'>
-              <h1 className='text-2xl mr-4'>Nişantaşı Üniversitesi / Yönetim Bilişim Sistemleri</h1>
-              <h1>(2019-2024)</h1>
+              <h1 className='text-2xl mr-4 max-sm:text-xl'>Nişantaşı Üniversitesi / Yönetim Bilişim Sistemleri</h1>
+              <h1 className='max-sm:absolute max-sm:invisible'>(2019-2024)</h1>
                 </span>
-                <h1 className='text-2xl'>Nişantaşı Üniversitesi / C1+ Advanced English</h1>
-              <h1> Eğitim hayatım boyunca çeşitli projelerde yer aldım, <br/>
+                <h1 className='text-2xl max-sm:absolute max-sm:invisible'>Nişantaşı Üniversitesi / C1+ Advanced English</h1>
+              <h1 className='max-sm:invisible max-sm:absolute'> Eğitim hayatım boyunca çeşitli projelerde yer aldım, <br/>
                   SQL ve .NET kullanarak Personel maaş takip programı geliştirdim. <br/>
                   React ile Tatil ve Uçak rezervasyonu uygulaması geliştirdim.</h1>
             </span>
           </div>
-          <div className='w-screen'>
-            <h1 className='text-3xl ml-24 font-display bg-white/90 w-max h-14 rounded-t-xl p-2 shadow-inner border-t'>Profesyonel Tecrübelerim :</h1>
-            <div className='hover:scale-105 transition-all flex items-center justify-around bg-white/90 rounded-2xl drop-shadow-md p-4 ml-8 mr-8 mb-4'>
-              <img src={Ito} className='w-96 h-48 drop-shadow-md shadow-inner rounded-2xl mr-12'/>
+          <div className='slider h-12'>
+            <div className='list flex gap-12'>
+              <span className='item item1 flex items-center justify-center gap-2 text-4xl text-[#ff5733]/50'><FaHtml5/>HTML</span>
+              <span className='item item2 flex items-center justify-center gap-2 text-4xl text-[#2965f1]/50'><FaCss3Alt/>CSS</span>
+              <span className='item item3 flex items-center justify-center gap-2 text-4xl text-[#f0db4f]/50'><BiLogoJavascript/>JavaScript</span>
+              <span className='item item4 flex items-center justify-center gap-2 text-4xl text-[#007acc]/50'><BiLogoTypescript/>Typescript</span>
+              <span className='item item5 flex items-center justify-center gap-2 text-4xl text-[#61DBFB]/50'><FaReact/>React</span>
+              <span className='item item6 flex items-center justify-center gap-2 text-4xl text-[#764abc]/50'><TbBrandRedux/>Redux</span>
+              <span className='item item7 flex items-center justify-center gap-2 text-4xl text-[#8892be]/50'><FaPhp/>PHP</span>
+              <span className='item item8 flex items-center justify-center gap-2 text-4xl text-[#512BD4]/50'><DiDotnet/>.NET</span>
+            </div>
+          </div>
+          <div>
+            <h1 id='tecrube' className='text-xl mt-12 ml-24 bg-white w-max h-10 rounded-t-xl p-2 shadow-inner border-t max-sm:ml-5 max-sm:rounded-full'>Profesyonel Tecrübelerim :</h1>
+            <div className='hover:scale-105 transition-all flex items-center justify-around bg-white drop-shadow-md p-4 mb-4 max-sm:flex-col w-screen'>
+              <img src={Ito} className='w-96 h-48 drop-shadow-md shadow-inner rounded-2xl mr-12 max-sm:mr-0'/>
               <span>
-                <div className='flex justify-between'>
-                  <h1 className='text-2xl'> İstanbul Ticaret Odası .NET Developer</h1>
+                <div className='flex justify-between max-sm:flex-col'>
+                  <h1 className='text-2xl mb-2'>İstanbul Ticaret Odası .NET Developer</h1>
                   <h1 className='text-xl'>(2 Ay Stajer)</h1>
                   </div>
-              <p className='w-[950px]'> &gt; .NET Core ve ASP.NET teknolojileriyle yazılım geliştirme süreçlerine aktif katılım sağladım. <br/>
+              <p className='w-[950px] max-sm:invisible max-sm:absolute'> &gt; .NET Core ve ASP.NET teknolojileriyle yazılım geliştirme süreçlerine aktif katılım sağladım. <br/>
               &gt; Veritabanı yönetimi için SQL Server kullanarak sorgu yazma ve veri işlemlerine katkı sundum. <br/>
               &gt; Web API geliştirme süreçlerinde backend işlemleri gerçekleştirdim ve RESTful API entegrasyonunu sağladım. <br/>
               &gt; Agile (Scrum) metodolojisi ile çalışan ekiplerde sprintlere katılarak yazılım geliştirme süreçlerini öğrendim. <br/>
@@ -44,14 +67,14 @@ const Main = () => {
               &gt; Modül entegrasyonu ve test edilmesi aşamalarında yazılımın doğruluğunu sağlamak için entegrasyon testleri gerçekleştirdim.</p>
               </span>
             </div>
-            <div className='hover:scale-105 transition-all flex items-center justify-around bg-white/90 rounded-2xl drop-shadow-md shadow-inner p-4 ml-8 mr-8'>
-              <img src={Raven} className='w-96 h-48 drop-shadow-md shadow-inner rounded-2xl mr-12'/>
+            <div className='hover:scale-105 transition-all flex items-center justify-around bg-white drop-shadow-md shadow-inner p-4 mb-12 max-sm:flex-col'>
+              <img src={Raven} className='w-96 h-48 drop-shadow-md shadow-inner rounded-2xl mr-12 max-sm:mr-0'/>
               <span>
-                <div className='flex justify-between'>
-                <h1 className='text-2xl'>Ravensoft React Developer</h1>
+                <div className='flex justify-between max-sm:flex-col'>
+                <h1 className='text-2xl mb-2'>Ravensoft React Developer</h1>
                 <h1 className='text-xl'>(6 Ay)</h1>
                 </div>
-                <p className='w-[950px]'>&gt; React.js ve Redux kullanarak dinamik e-ticaret web siteleri geliştirdim ve mevcut projelere özellikler ekledim. <br/>
+                <p className='w-[950px] max-sm:invisible max-sm:absolute'>&gt; React.js ve Redux kullanarak dinamik e-ticaret web siteleri geliştirdim ve mevcut projelere özellikler ekledim. <br/>
               &gt; Kullanıcı dostu arayüzler oluşturmak için HTML, CSS ve JavaScript ile responsive ve modern tasarımlar gerçekleştirdim. <br/>
               &gt; E-ticaret sitesi için ürün listeleme, sepet, ödeme işlemleri gibi kritik işlevlerin geliştirilmesini sağladım.  <br/>
               &gt; RESTful API'ler ile veri entegrasyonu yaparak ürün bilgilerini, kullanıcı sepetlerini ve sipariş durumlarını dinamik olarak çektim. <br/>
@@ -61,53 +84,48 @@ const Main = () => {
             </div>
           </div>
         </div>
-        <div>
-          <h1 className='text-3xl ml-24 mt-12 font-display bg-white/90 w-max h-14 rounded-t-xl p-2 shadow-inner border-t'>Freelance Projelerim :</h1>
-          <div className='hover:scale-105 transition-all flex items-center justify-around bg-white/90 rounded-2xl drop-shadow-md p-4 ml-8 mr-8'>
+        <div className='w-screen'>
+          <h1 className='text-xl ml-24 mt-12 w-max h-10 rounded-t-xl bg-w bg-white p-2 shadow-inner border-t max-sm:ml-16' id='freelance' >Freelance Projelerim :</h1>
+          <div className='hover:scale-105 transition-all flex items-center justify-around bg-white drop-shadow-md max-sm:flex-col-reverse'>
               <span>
-                <div className='flex justify-between'>
-                  <h1 className='text-2xl'>Türkiye Katılım Bankaları Birliği Websitesi:</h1>
+                <div className='flex justify-between max-sm:flex-col'>
+                  <h1 className='text-2xl mb-2'>Türkiye Katılım Bankaları Birliği Websitesi:</h1>
                   <h1 className='text-xl'>(Destek)</h1>
                   </div>
-              <p className='w-[950px]'> &gt; .NET Core ve ASP.NET teknolojileriyle yazılım geliştirme süreçlerine aktif katılım sağladım. <br/>
-              &gt; Veritabanı yönetimi için SQL Server kullanarak sorgu yazma ve veri işlemlerine katkı sundum. <br/>
+              <p className='w-[950px] max-sm:invisible max-sm:absolute'> &gt; Web uygulamaları ve kullanıcı arayüzleri oluşturmak için React.js framework'ünü kullanarak komponentler geliştirdim <br/>
+              &gt; React Hooks ve Class Components kullanarak fonksiyonel ve sınıf tabanlı komponentler oluşturdum. <br/>
               &gt; Web API geliştirme süreçlerinde backend işlemleri gerçekleştirdim ve RESTful API entegrasyonunu sağladım. <br/>
-              &gt; Agile (Scrum) metodolojisi ile çalışan ekiplerde sprintlere katılarak yazılım geliştirme süreçlerini öğrendim. <br/>
-              &gt; Kullanıcı arayüzü (UI) tasarımlarında HTML, CSS ve JavaScript ile uygulama geliştirme süreçlerine katkı sağladım. <br/>
-              &gt; Modül entegrasyonu ve test edilmesi aşamalarında yazılımın doğruluğunu sağlamak için entegrasyon testleri gerçekleştirdim.</p>
+              &gt; Kullanıcı arayüzü (UI) tasarımlarında HTML, CSS ve JavaScript ile uygulama geliştirme süreçlerine katkı sağladım.</p>
               </span>
-              <img src={TKBB} className='w-96 drop-shadow-md shadow-inner rounded-2xl mr-12 w-96 h-48'/>
+              <img src={TKBB} className='w-96 drop-shadow-md shadow-inner rounded-2xl mr-12 w-96 h-48 max-sm:mr-0'/>
             </div>
-            <div className='hover:scale-105 transition-all flex items-center justify-around bg-white/90 rounded-2xl drop-shadow-md shadow-inner p-4 m-8'>
+            <div className='hover:scale-105 transition-all flex items-center justify-around bg-white drop-shadow-md shadow-inner p-4 mt-8 max-sm:flex-col-reverse'>
               <span>
                 <div className='flex justify-between'>
-                <h1 className='text-2xl'>Sağlık Bilimleri Üniversitesi Websitesi</h1>
+                <h1 className='text-2xl mb-2'>Sağlık Bilimleri Üniversitesi Websitesi</h1>
                 <h1 className='text-xl'>(Destek)</h1>
                 </div>
-                <p className='w-[950px]'>&gt; React.js ve Redux kullanarak dinamik e-ticaret web siteleri geliştirdim ve mevcut projelere özellikler ekledim. <br/>
-              &gt; Kullanıcı dostu arayüzler oluşturmak için HTML, CSS ve JavaScript ile responsive ve modern tasarımlar gerçekleştirdim. <br/>
-              &gt; E-ticaret sitesi için ürün listeleme, sepet, ödeme işlemleri gibi kritik işlevlerin geliştirilmesini sağladım.  <br/>
-              &gt; RESTful API'ler ile veri entegrasyonu yaparak ürün bilgilerini, kullanıcı sepetlerini ve sipariş durumlarını dinamik olarak çektim. <br/>
+                <p className='w-[950px] max-sm:invisible max-sm:absolute'>&gt; React.js ve Redux kullanarak dinamik olarak komponentler geliştirdim.<br/>
+              &gt; Uygulama genelinde veri yönetimi için Redux, Context API gibi araçlar kullanarak global state yönetimi sağladım. <br/>
               &gt; Arama, filtreleme ve sıralama gibi özelliklerini geliştirmek için React state yönetimi ve component yapısını optimize ettim. <br/>
-              &gt; Hataların tespiti ve çözümü için Jest ve React Testing Library kullanarak birim testleri ve entegrasyon testleri gerçekleştirdim.</p>
+              &gt; Uygulama bileşenlerinin birbirleriyle olan etkileşimlerini ve lifecycle işlemlerini yönetmek.</p>
               </span>
-              <img src={SBU} className='w-96 drop-shadow-md shadow-inner rounded-2xl mr-12'/>
+              <img src={SBU} className='w-96 drop-shadow-md shadow-inner rounded-2xl mr-12 max-sm:mr-0'/>
             </div>
-            <div className='hover:scale-105 transition-all flex items-center justify-around bg-white/90 rounded-2xl drop-shadow-md shadow-inner p-4 m-8'>
+            <div className='hover:scale-105 transition-all flex items-center justify-around bg-white drop-shadow-md shadow-inner mt-8 mb-8 p-4 max-sm:flex-col-reverse'>
               <span>
                 <div className='flex justify-between'>
-                <h1 className='text-2xl'>Payna Yazılım ve Danışmanlık Websitesi</h1>
+                <h1 className='text-2xl mb-2'>Payna Yazılım ve Danışmanlık Websitesi</h1>
                 <h1 className='text-xl'>(Teslim)</h1>
                 </div>
-                <p className='w-[950px]'>&gt; React.js ve Redux kullanarak dinamik e-ticaret web siteleri geliştirdim ve mevcut projelere özellikler ekledim. <br/>
-              &gt; Kullanıcı dostu arayüzler oluşturmak için HTML, CSS ve JavaScript ile responsive ve modern tasarımlar gerçekleştirdim. <br/>
-              &gt; E-ticaret sitesi için ürün listeleme, sepet, ödeme işlemleri gibi kritik işlevlerin geliştirilmesini sağladım.  <br/>
-              &gt; RESTful API'ler ile veri entegrasyonu yaparak ürün bilgilerini, kullanıcı sepetlerini ve sipariş durumlarını dinamik olarak çektim. <br/>
-              &gt; Arama, filtreleme ve sıralama gibi özelliklerini geliştirmek için React state yönetimi ve component yapısını optimize ettim. <br/>
-              &gt; Hataların tespiti ve çözümü için Jest ve React Testing Library kullanarak birim testleri ve entegrasyon testleri gerçekleştirdim.</p>
+                <p className='w-[950px] max-sm:invisible max-sm:absolute'>&gt; Kullanıcı deneyimi için UI kitleri veya özel stiller kullanarak görsel olarak çekici ve kullanıcı dostu arayüzler oluşturdum. <br/>
+              &gt; Web sayfalarının mobil uyumlu ve responsive olmasını sağladım. (Media Queries, Flexbox, Grid).<br/>
+              &gt; React bileşenlerini test etmek için Jest, React Testing Library gibi araçlarla unit ve integration testleri yazdım.<br/>
+              &gt; React uygulamaları için SEO uyumluluğu sağladım. (Server-Side Rendering, Static Site Generation gibi yöntemlerle).</p>
               </span>
-              <img src={Payna} className='w-96 h-48  drop-shadow-md shadow-inner rounded-2xl mr-12'/>
+              <img src={Payna} className='w-96 h-48  drop-shadow-md shadow-inner rounded-2xl mr-12 max-sm:mr-0'/>
             </div>
+          </div>
         </div>
     </div>
   )
