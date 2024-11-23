8eb6f61e9510a044a441242f8b0ca145315afe3e
efeyilmazer <84992900+efeyilmazer@users.noreply.github.com>
2024-11-24 00:22:14 +0300

    updated

--- mini-portfolio/src/pages/Projelerim.jsx
+++ mini-portfolio/src/pages/Projelerim.jsx
@@ -63,7 +63,7 @@ const Projelerim = () => {
       <h1 className='text-4xl mt-12 font-display mb-4'>Projelerim</h1>
       <span className='max-w-[1000px] text-center'>
       <p className='text-xl'>Beş sene içerisinde kendimi geliştirmek için oluşturduğum projelerin görsel ve kaynak kodları, Projelerde kullandığım teknolojiler tamamen karışık şekilde HTML Css JS ve React ile oluşmaktadır.</p>
-      <p className='text-xl p-4'>Tüm kaynak kodları github linkinde mevcuttur.</p>
+      <p className='text-xl p-4'>Tüm kaynak kodları github linkimde mevcuttur.</p>
       </span>
       <span className='w-[750px] h-2 bg-slate-700/20 mt-4 mb-16 rounded-full'></span>
       </div>
@@ -76,9 +76,9 @@ const Projelerim = () => {
         </div>
 
         <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
-          <div><img className='w-96 mb-8 rounded-xl' src={Pj1} alt="" /></div>
-          <h1 className='text-2xl mb-2'>Background Slider</h1>
-          <p className='text-center'>Slider'a tıkladığınızda sliderdaki görsel arkaplanla birlikte değişir</p>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj6} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Payment Page UI</h1>
+          <p className='text-center'>Ödeme planlayan, tarihleyen ve ödeme seçeneklerini sunan bir sayfa örneği</p>
         </div>
 
         <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
@@ -100,15 +100,15 @@ const Projelerim = () => {
         </div>
 
         <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
-          <div><img className='w-96 mb-8 rounded-xl' src={Pj6} alt="" /></div>
-          <h1 className='text-2xl mb-2'>Payment Page UI</h1>
-          <p className='text-center'>Ödeme planlayan, tarihleyen ve ödeme seçeneklerini sunan bir sayfa örneği</p>
-        </div>
-
-        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
           <div><img className='w-96 mb-8 rounded-xl' src={Pj7} alt="" /></div>
           <h1 className='text-2xl mb-2'>Pokedex</h1>
           <p className='text-center'>Aramayı kullandığınızda API'dan anahtar kelimeye sahip gerekli pokemonları anlık olarak getirir</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj1} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Background Slider</h1>
+          <p className='text-center'>Slider'a tıkladığınızda sliderdaki görsel arkaplanla birlikte değişir</p>
         </div>
 
         <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
@@ -162,7 +162,211 @@ const Projelerim = () => {
         <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
           <div><img className='w-96 mb-8 rounded-xl' src={Pj18} alt="" /></div>
           <h1 className='text-2xl mb-2'>Scroll Animation</h1>
-          <p className='text-center'>Sayfayı aşağı kaydırdıkça ekrana görseller ve divler gelmesini sağlayan görsel animasyonlara sahip bir proje.</p>
+          <p className='text-center'>Sayfayı aşağı kaydırdıkça ekrana görseller gelmesini sağlayan görsel animasyonlara sahip bir proje.</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj20} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Sound Board</h1>
+          <p className='text-center'>Yazılan sesi bilgisayarınızda çalan görsel ve işitsel bir uygulama</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj21} alt="" /></div>
+          <h1 className='text-2xl mb-2'>To-Do App</h1>
+          <p className='text-center'>Ekleme ve Çıkartma, Planlama ve Programlama yapabilen anımsatıcılı bir proje</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj22} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Keyboard Event Catcher</h1>
+          <p className='text-center'>Klavyeden giriş yapılan tuşun value değerlerini ekrana yazdıran bir proje</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj23} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Animated Loading</h1>
+          <p className='text-center'>Görsel olarak güzel bir animasyona sahip loading denemesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj24} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Dad Joke Generator</h1>
+          <p className='text-center'>Butona tıkladığınızda API'den soğuk bir espri ekrana getiren uygulama</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj25} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Countdown</h1>
+          <p className='text-center'>Sayfanın açılışından başlayan ve yenileseniz bile sıfırlanmayan bir countdown projesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj26} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Blurry Loading</h1>
+          <p className='text-center'>Sayfanın açılışında JS verilerinin yüklenmesini görsel blurla destekledim.</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj27} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Day/Night Theme</h1>
+          <p className='text-center'>Sayfayı aydınlık ve karanlık temaya değiştiren bir button projesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj28} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Insta Like</h1>
+          <p className='text-center'>İnstagramdaki çift tıklayınca çıkan kalpi ve kaç kere beğenildiğini göstermeyi başardığım proje</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj29} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Good Cheap Fast</h1>
+          <p className='text-center'>Bir projeni İyi ve hızlı olmasını isteyebilirsiniz ama aynı zamanda ucuz olmasını bekleyemezsiniz :)</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj30} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Hidden Input</h1>
+          <p className='text-center'>Arama butonuna bastığınızda açılan ve yazı girildikten sonra tekrar kapanan input</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj31} alt="" /></div>
+          <h1 className='text-2xl mb-2'>FAQ Project</h1>
+          <p className='text-center'>Frequently Asked Questions temasını denediğim bir UI projesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj32} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Drag&Drop</h1>
+          <p className='text-center'>Görseli tutup istediğiniz boşluğa koyabildiğiniz ve hafızası olan bir proje</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj33} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Wikipedia UI</h1>
+          <p className='text-center'>Wikipedia ülkemizde yasaklandığında sinirlenip kopyalamaya çalıştığım proje</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj34} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Settings UI</h1>
+          <p className='text-center'>Potansiyel bir settings sayfasına ait herşeyi denedim ve yaptım</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj35} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Social Links UI</h1>
+          <p className='text-center'>Sosyal medya ve linkedin github vb. linklerin bulunduğu bir geliştirme</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj36} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Linkedin-Like UI</h1>
+          <p className='text-center'>Linkedin benzeri bir iş bulma ve filtreleme uygulaması denemesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj37} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Udemy-Like UI</h1>
+          <p className='text-center'>Udemy benzeri bir ders bulma ve filtreme uygulaması denemesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj38} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Modal UI</h1>
+          <p className='text-center'>Tipik her sayfada gördüğümüz login modal'ı UI olarak denedim</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj39} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Animated Cookies</h1>
+          <p className='text-center'>Sayfalarda ilk açılışta çıkan cookie butonlarını animasyon haline getirdim.</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj40} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Footer UI</h1>
+          <p className='text-center'>Sayfalardaki tipik Footer tarzını kopyalamaya ve görsel olarak güzel birşeyler yapmaya çalıştım</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj41} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Color Palette Generator</h1>
+          <p className='text-center'>Butona bastığınızda rastgele renkler oluşturur ve tıkladığınızda hex kodunu kopyalar</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj42} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Payment Plan UI</h1>
+          <p className='text-center'>E-ticaret sayfalarındaki ödeme planını kopyalamaya ve görsel olarak güzel hale getirmeyi denedim</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj43} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Charity Goal UI</h1>
+          <p className='text-center'>Birisi ödeme yaptığında hedef miktara ne kadar kaldığını gösteren uygulama</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj44} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Share Button UI</h1>
+          <p className='text-center'>Sayfalarda ve uygulamalardaki tipik share butonlarını kopyaladım </p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj45} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Payment Plan UI</h1>
+          <p className='text-center'>Sayfalardaki tipik ödeme planlayıcısını kopyaladım ve kendimce görselleştirdim</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj46} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Payment Discount UI</h1>
+          <p className='text-center'>Sayfalardaki ödeme planlayıcısında indirim olduğunda otomatik olarak onu seçen bir uygulama</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj47} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Blog Cards UI</h1>
+          <p className='text-center'>Blog sayfalarındaki tipik kart görsellerini kendimce kodladım</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj48} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Help Center UI</h1>
+          <p className='text-center'>Faq sayfası gibi bir help center ve görsel cardlar UI projesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj49} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Rate App UI</h1>
+          <p className='text-center'>Uygulamayı ve hizmeti puanlayabildiğimiz bir UI geliştirmesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj50} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Support Page UI</h1>
+          <p className='text-center'>Bir sorunla karşılaştığınızda gireceğiniz support sayfasını basitçe kodladım</p>
+        </div>
+        
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj51} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Search Filter UI</h1>
+          <p className='text-center'>Inputta aramak istediğiniz şeyi filtreleyebileceğiniz bir UI geliştirmesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj31} alt="" /></div>
+          <h1 className='text-2xl mb-2'>FAQ UI</h1>
+          <p className='text-center'>Sıkça Sorulan sorular kısmının görsel olarak basit bir UI geliştirmesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj52} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Random UI</h1>
+          <p className='text-center'>Bir sayfada karşıma çıkan ve rastgele UI denemesi yaptığım bir proje</p>
         </div>
       </div>
     </div>
