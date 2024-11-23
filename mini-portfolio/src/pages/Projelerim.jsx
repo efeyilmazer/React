2bc00036e91fc117e6de159911c71461029011bb
efeyilmazer <84992900+efeyilmazer@users.noreply.github.com>
2024-11-23 23:51:45 +0300

    added

--- mini-portfolio/src/pages/Projelerim.jsx
+++ mini-portfolio/src/pages/Projelerim.jsx
@@ -1,10 +1,170 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import  Pj1 from "../img/1.png"
+import  Pj2 from "../img/2.png"
+import  Pj3 from "../img/3.png"
+import  Pj4 from "../img/4.png"
+import  Pj5 from "../img/5.png"
+import  Pj6 from "../img/6.png"
+import  Pj7 from "../img/7.png"
+import  Pj8 from "../img/8.png"
+import  Pj9 from "../img/1.png"
+import  Pj10 from "../img/9.png"
+import  Pj11 from "../img/10.png"
+import  Pj12 from "../img/11.png"
+import  Pj13 from "../img/12.png"
+import  Pj14 from "../img/13.png"
+import  Pj15 from "../img/14.png"
+import  Pj16 from "../img/16.png"
+import  Pj17 from "../img/17.png"
+import  Pj18 from "../img/18.png"
+import  Pj19 from "../img/10.png"
+import  Pj20 from "../img/20.png"
+import  Pj21 from "../img/21.png"
+import  Pj22 from "../img/22.png"
+import  Pj23 from "../img/23.png"
+import  Pj24 from "../img/24.png"
+import  Pj25 from "../img/25.png"
+import  Pj26 from "../img/26.png"
+import  Pj27 from "../img/27.png"
+import  Pj28 from "../img/28.png"
+import  Pj29 from "../img/29.png"
+import  Pj30 from "../img/30.png"
+import  Pj31 from "../img/31.png"
+import  Pj32 from "../img/32.png"
+import  Pj33 from "../img/33.png"
+import  Pj34 from "../img/34.png"
+import  Pj35 from "../img/35.png"
+import  Pj36 from "../img/36.png"
+import  Pj37 from "../img/37.png"
+import  Pj38 from "../img/38.png"
+import  Pj39 from "../img/39.png"
+import  Pj40 from "../img/40.png"
+import  Pj41 from "../img/41.png"
+import  Pj42 from "../img/42.png"
+import  Pj43 from "../img/43.png"
+import  Pj44 from "../img/44.png"
+import  Pj45 from "../img/45.png"
+import  Pj46 from "../img/46.png"
+import  Pj47 from "../img/47.png"
+import  Pj48 from "../img/48.png"
+import  Pj49 from "../img/49.png"
+import  Pj50 from "../img/50.png"
+import  Pj51 from "../img/51.png"
+import  Pj52 from "../img/52.png"
+
+
 
 const Projelerim = () => {
   return (
     <div>
       <Navbar/>
+      <div className='flex flex-col items-center justify-center'>
+      <h1 className='text-4xl mt-12 font-display mb-4'>Projelerim</h1>
+      <span className='max-w-[1000px] text-center'>
+      <p className='text-xl'>Beş sene içerisinde kendimi geliştirmek için oluşturduğum projelerin görsel ve kaynak kodları, Projelerde kullandığım teknolojiler tamamen karışık şekilde HTML Css JS ve React ile oluşmaktadır.</p>
+      <p className='text-xl p-4'>Tüm kaynak kodları github linkinde mevcuttur.</p>
+      </span>
+      <span className='w-[750px] h-2 bg-slate-700/20 mt-4 mb-16 rounded-full'></span>
+      </div>
+
+      <div className='flex gap-12 flex-wrap items-center justify-center'>
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj8} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Film-Dizi Sitesi</h1>
+          <p className='text-center'>Aramayı kullandığınızda API'dan anahtar kelimeye sahip gerekli dizi-filmleri getirir</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj1} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Background Slider</h1>
+          <p className='text-center'>Slider'a tıkladığınızda sliderdaki görsel arkaplanla birlikte değişir</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj3} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Expanding Pages</h1>
+          <p className='text-center'>Mouse'yi hangi tarafa götürürseniz o tarafın görseli diğerini bastırarak büyür</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj4} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Cross Slider Projesi</h1>
+          <p className='text-center'>Slider'a görsellik katmak için görsel değiştiğinde ilgili görselin rengine sahip bir kart zıt yönden gelir</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj5} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Expanding Cards</h1>
+          <p className='text-center'>Tıkladığınız görsel ekranın belli bir bölümünü kaplayacak şekilde genişler</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj6} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Payment Page UI</h1>
+          <p className='text-center'>Ödeme planlayan, tarihleyen ve ödeme seçeneklerini sunan bir sayfa örneği</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj7} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Pokedex</h1>
+          <p className='text-center'>Aramayı kullandığınızda API'dan anahtar kelimeye sahip gerekli pokemonları anlık olarak getirir</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj10} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Animated Navbar</h1>
+          <p className='text-center'> Butona tıkladığınızda açılıp-kapanma efektine sahip bir navbar geliştirmesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj11} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Modal Login UI</h1>
+          <p className='text-center'>Login sayfasına ve register sayfasına sahip bir UI geliştirmesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj12} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Lamp With Shadows</h1>
+          <p className='text-center'>Lambaya bastığınızda sayfayı ve altındaki öğeyi gölgelendirecek şekilde açılan lamba.</p>
+        </div>
+        
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj13} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Calculator</h1>
+          <p className='text-center'>Yardım almadan JS ile geliştirdiğim basit bir hesap makinesi projesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj14} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Rotating Nav</h1>
+          <p className='text-center'>Butona bastığınızda tüm sayfayı kaydırıp arkasındaki gizli menüyü ekrana getiren görsel animasyonlu bir UI</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj15} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Mobil Navigation</h1>
+          <p className='text-center'>İnstagram gibi uygulamaların alt menülerini denediğim navigation bar</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj16} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Random Selector</h1>
+          <p className='text-center'>Girdiğiniz kelimeleri hafızaya ayıp 10 saniye sonra rastgele birini seçen bir uygulama</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj17} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Day/Night Clock UI</h1>
+          <p className='text-center'>Aydınlık ve Karanlık temaya sahip görsel animasyonlu bir saat projesi</p>
+        </div>
+
+        <div className='w-[450px] h-96 bg-white p-8 rounded-xl flex flex-col items-center justify-center'>
+          <div><img className='w-96 mb-8 rounded-xl' src={Pj18} alt="" /></div>
+          <h1 className='text-2xl mb-2'>Scroll Animation</h1>
+          <p className='text-center'>Sayfayı aşağı kaydırdıkça ekrana görseller ve divler gelmesini sağlayan görsel animasyonlara sahip bir proje.</p>
+        </div>
+      </div>
     </div>
   )
 }
